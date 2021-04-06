b3447886b9841acc5153d019dcf5a12ac0f2caf2
abdulwahab786 <42349896+abdulwahab786@users.noreply.github.com>
2021-04-06 10:31:56 -0700

    update single file

--- octocs/single.php
+++ octocs/single.php
@@ -2,6 +2,10 @@
 
 get_header();
 ?>
+
+<?php if ( is_active_sidebar( 'custom-side-bar' ) ) : ?>
+    <?php dynamic_sidebar( 'custom-side-bar' ); ?>
+<?php endif; ?>
     
     
 		<article class="content px-3 py-5 p-md-5">
